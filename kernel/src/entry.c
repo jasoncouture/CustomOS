0b08410e7f2a788fc02cc3ece27e23aaad09334f
Jason Couture <jasonc@alertr.info>
2021-03-02 20:46:58 -0500

    Added a comment about my laziness

--- kernel/src/entry.c
+++ kernel/src/entry.c
@@ -42,6 +42,10 @@ void DrawPixel(unsigned int x, unsigned int y, unsigned int color)
 void _start(KernelParameters *kernelParameters)
 {
     kInitializeFrameBuffer(kernelParameters->FrameBuffer);
+    // Color format is RESERVED- B G R, each is 8 bits
+    // This is converted during the function call DirectWritePixel for GOP pixel mode 1 and 2 (See graphics/framebuffer.c)
+    // Othermodes will result in framebuffer operations becoming a no-op. Because I'm far too lazy to implement anything
+    // that isn't 24/32 bit color with 8BPP
     unsigned int colors[3] = {0xFF00FF00, 0xFF0000FF, 0xFFFF0000};
     unsigned int colorIndex = 0;
     while (1)
