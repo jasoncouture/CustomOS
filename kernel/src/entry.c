78d0f17e40d56490a42f034769e7824f0014a3e9
Jason Couture <jasonc@alertr.info>
2021-03-03 00:16:43 -0500

    Remove the screen color fill code used to test kSetPixel

--- kernel/src/entry.c
+++ kernel/src/entry.c
@@ -21,23 +21,4 @@ void _start(KernelParameters *kernelParameters)
     kPutCharacterWithColor('o', 32, 0, GREEN, BLACK);
 
     while(1);
-    // Color format is RESERVED- B G R, each is 8 bits
-    // This is converted during the function call DirectWritePixel for GOP pixel mode 1 and 2 (See graphics/framebuffer.c)
-    // Othermodes will result in framebuffer operations becoming a no-op. Because I'm far too lazy to implement anything
-    // that isn't 24/32 bit color with 8BPP
-    unsigned int colors[3] = {0xFF00FF00, 0xFF0000FF, 0xFFFF0000};
-    unsigned int colorIndex = 0;
-
-    while (1)
-    {
-        unsigned int selectedColor = colors[colorIndex];
-        colorIndex = (colorIndex + 1) % 3;
-        for (unsigned int y = 0; y < kernelParameters->FrameBuffer->Height; y++)
-        {
-            for (unsigned int x = 0; x < kernelParameters->FrameBuffer->Width; x++)
-            {
-                kSetPixel(x, y, selectedColor);
-            }
-        }
-    }
 }
