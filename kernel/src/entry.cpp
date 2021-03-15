aa769812b4bf16f2af2f49a45a26787f2daa14de
Jason Couture <jasonc@alertr.info>
2021-03-14 20:20:09 -0400

    Pretty up the event debug printing

--- kernel/src/entry.cpp
+++ kernel/src/entry.cpp
@@ -29,16 +29,40 @@ void WriteDebugData(const char *description, uint64_t value, uint64_t lineNumber
     const char *numericString = hex ? kToHexString(value) : kToString(value);
     font->DrawStringAt(numericString, font->GetCharacterPixelWidth() * 30, font->GetCharacterPixelHeight() * lineNumber);
 }
-
+const double interval = 1.0/60.0;
 void OnEvent(Event *event)
 {
-    WriteDebugData("Event:", event->EventId(), 2);
-    WriteDebugData("Event Data:", event->EventData(), 3);
-    auto eventLoop = Kernel::Events::EventLoop::GetInstance();
-    WriteDebugData("Timestamp:", (uint64_t)(Kernel::Timer::GetInstance()->ElapsedTime() * 1000), 4);
-    WriteDebugData("Pending events:", eventLoop->Pending(), 5);
+    static uint64_t eventCount = 0;
+    static uint64_t eventCountAtLastScreenUpdate;
+    static uint64_t maxPending = 0;
+    static double lastScreenUpdate = 0.0;
+    static Kernel::Events::EventLoop* eventLoop = NULL;
+    if(eventLoop == NULL)
+        eventLoop = Kernel::Events::EventLoop::GetInstance();
+    auto pendingEvents = eventLoop->Pending();
+    if(pendingEvents > maxPending)
+        maxPending = pendingEvents;
+    eventCount++;
     
     DispatchKernelEvent(event);
+    if (event->EventId() == EventType::TimerTick)
+    {
+        auto currentTime = Kernel::Timer::GetInstance()->ElapsedTime();
+        if (currentTime - lastScreenUpdate >= interval)
+        {
+            auto eventsSinceLastUpdate = eventCount - eventCountAtLastScreenUpdate;
+            eventCountAtLastScreenUpdate = eventCount;
+            WriteDebugData("Event:", event->EventId(), 2);
+            WriteDebugData("Event Data:", event->EventData(), 3);
+            WriteDebugData("Timestamp (MS):", (uint64_t)(Kernel::Timer::GetInstance()->ElapsedTime() * 1000), 4);
+            WriteDebugData("Timestamp (Seconds):", (uint64_t)(Kernel::Timer::GetInstance()->ElapsedTime()), 5);
+            WriteDebugData("Pending events:", pendingEvents, 6);
+            WriteDebugData("Max Pending events:", maxPending, 7);
+            WriteDebugData("Events:", eventCount, 8);
+            WriteDebugData("Events per second:", (uint64_t)((double)eventsSinceLastUpdate / (currentTime - lastScreenUpdate)), 9);
+            lastScreenUpdate = currentTime;
+        }
+    }
 }
 
 void kMain(KernelParameters *kernelParameters)
@@ -52,8 +76,8 @@ void kMain(KernelParameters *kernelParameters)
     font->DrawStringAt("Frame buffer initialized and console font loaded", 0, font->GetCharacterPixelHeight() * 1);
 
     auto bitmap = pageAllocator->GetBitmap();
-    WriteDebugData("Bitmap located at:", (uint64_t)bitmap->GetBuffer(), 6, true);
-    WriteDebugData("Bitmap size:", (uint64_t)bitmap->Size(), 7);
+    WriteDebugData("Bitmap located at:", (uint64_t)bitmap->GetBuffer(), 20, true);
+    WriteDebugData("Bitmap size:", (uint64_t)bitmap->Size(), 21);
     auto eventLoop = Kernel::Events::EventLoop::GetInstance();
     eventLoop->Publish(new Event(EventType::TimerTick, 0));
     eventLoop->Publish(new Event(EventType::TimerTick, 1));
