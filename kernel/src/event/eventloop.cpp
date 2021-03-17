6b9decc23be07e37355ab1ad4bd42b9b6f1cece2
Jason Couture <jasonc@alertr.info>
2021-03-17 14:40:19 -0400

    Add the ability to hook events rather than handle all events

--- kernel/src/event/eventloop.cpp
+++ kernel/src/event/eventloop.cpp
@@ -5,13 +5,17 @@ using namespace Kernel::Collections;
 
 EventLoop *EventLoop::GetInstance()
 {
-    if (GlobalEventLoop == NULL)
-        GlobalEventLoop = new EventLoop();
-    return GlobalEventLoop;
+    asm volatile ("cli");
+    
+    if (EventLoop::GlobalEventLoop == NULL)
+        EventLoop::GlobalEventLoop = new EventLoop();
+    asm volatile ("sti");
+    return EventLoop::GlobalEventLoop;
 }
 EventLoop::EventLoop()
 {
     this->eventQueue = new Queue<Event *>(4);
+    this->eventHooks = new Map<EventType, void (*)(Event *)>();
 }
 EventLoop *EventLoop::GlobalEventLoop = NULL;
 void EventLoop::Run(void (*onEvent)(Event *))
@@ -26,9 +30,19 @@ void EventLoop::Run(void (*onEvent)(Event *))
             asm("hlt"); // Wait for an interrupt to wake us up.
             continue;
         }
-        asm("sti");
-        onEvent(next);
+        
+        if(onEvent != NULL)
+            onEvent(next);
+        void (*handler)(Event *);
+        if (this->eventHooks->TryGet((EventType)next->EventId(), &handler))
+        {
+            if (handler != NULL)
+            {
+                handler(next);
+            }
+        }
         delete next;
+        asm("sti");
     }
 }
 
@@ -40,4 +54,15 @@ void EventLoop::Publish(Event *event)
 uint64_t EventLoop::Pending()
 {
     return this->eventQueue->Count();
+}
+
+void EventLoop::SetHandler(EventType eventType, void (*handler)(Event *))
+{
+    this->eventHooks->Remove(eventType);
+    this->eventHooks->Add(eventType, handler);
+}
+
+void EventLoop::ClearHandler(EventType eventType) 
+{
+    this->eventHooks->Remove(eventType);
 }
