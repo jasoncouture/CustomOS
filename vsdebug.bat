ae1036b3d0901e4f1b03b210c4cdaba511ab7567
Jason Couture <jasonc@alertr.info>
2021-03-21 18:21:47 -0400

    Use the new ISO file for debugging

--- vsdebug.bat
+++ vsdebug.bat
@@ -3,6 +3,6 @@ set BUILDDIR=bin
 set OVMFDIR=OVMFbin
 set PATH=C:\Program Files\qemu;%PATH%
 
-start "QEMU" /I qemu-system-x86_64.exe -s -S -monitor stdio -drive file=%BUILDDIR%\%OSNAME%.img,format=raw -m 512m -cpu qemu64 -drive if=pflash,format=raw,unit=0,file=%OVMFDIR%\OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=%OVMFDIR%\OVMF_VARS-pure-efi.fd -net none -L "C:\Program Files\qemu"
+start "QEMU" /I qemu-system-x86_64.exe -s -S -monitor stdio -cdrom %BUILDDIR%\%OSNAME%.iso -m 512m -cpu qemu64 -drive if=pflash,format=raw,unit=0,file=%OVMFDIR%\OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=%OVMFDIR%\OVMF_VARS-pure-efi.fd -net none -L "C:\Program Files\qemu"
 
 timeout /t 1 /nobreak 
